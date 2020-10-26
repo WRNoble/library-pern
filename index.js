ee08b4f2a78d0d37962eabeafa8603c558737e7b
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-26 11:27:55 -0400

    installed cors

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+//Middleware
+app.use(cors());
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
